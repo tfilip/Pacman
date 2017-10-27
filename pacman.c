2ad27b2903df413b15bd328e5423b5220218c14b
Filip Tepes-Onea <filip.tepes@outlook.com>
2017-10-27 10:59:21 +0300

    First Version

--- pacman.c
+++ pacman.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main(){
+void desenare_tabla(int N,int M,int x,int y,int P,char direction){
+
+	//Desenare stelute de sus
+	for(int i=0;i<N+2;i++){
+		if(i==0||i==N+2){
+			for(int j=0;j<M;j++)
+				printf("%s", "*");
+			continue;
+		}
+	}
+
+	
+}
+	
+	int main(){
+	
+		int N,M,x,y,P;
+	
+		scanf("%d%d%d%d%d",&N,&M,&x,&y,&P);
+	
+	)
 
 	return 0;
 }
--- task1.c
+++ task1.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
-int main(){
+/*
+    Preiau un argument si returnez valoarea sa dubla in spatii
+    pentru a lua in considerare si spatiile dintre caracter si
+    caracterul cerut */
 
-	return 0;
+void print_spaces(int n){
+    for(int i=0;i<2*n;i++){
+        printf("%s"," ");
+    }
+}
+
+
+
+void print_char(int n,char c){
+    for(int i=0;i<n;i++){
+        printf("%c ",c);
+    }
+}
+
+int main()
+{
+    int N;
+    char C;
+
+    scanf("%d %c",&N,&C);
+
+    for(int i=N;i>0;i--){
+        print_spaces(N-i);
+        print_char(i,C);
+        printf("\n");
+    }
+
+    return 0;
 }
